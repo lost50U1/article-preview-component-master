a3dec7b66028efa9c6397108307d3e3f8e2fb10b
lost50U1 <91626314+lost50U1@users.noreply.github.com>
2024-06-29 20:15:03 +0300

    first stage

--- src/js/main.js
+++ src/js/main.js
@@ -3,30 +3,30 @@ const footerContainer = document.getElementById("footer");
 
 btn.addEventListener("click", () => {
   footerContainer.style.display = "none";
+  /* create two variables with 1 with mobile layout 2 with desktop layout and make it check for sm: class with if else
+    method two create two footers with different id and create another footer variable
+    and improving it instead of using template literal make two footers one for mobile and one for
+    desktop on the html and make it none and block with js and by adding hidden class using tailwind*/
   footerContainer.innerHTML = `
-  <div id="footer">
-        <div class="flex justify-between items-center w-full bg-VeryDarkGrayishBlue px-8 py-4">
+ <div id="footer">
+        <div
+          class="flex justify-between items-center w-full bg-VeryDarkGrayishBlue px-8 py-4"
+        >
           <div class="flex items-center gap-5">
             <div>
-              <img
-                class="rounded-full h-auto w-10"
-                src="images/avatar-michelle.jpg"
-                alt=""
-              />
+              <p class="text-DesaturatedDarkBlue">S&nbsp;&nbsp;H&nbsp;&nbsp;A&nbsp;&nbsp;R&nbsp;&nbsp;E</p>
             </div>
-
-            <div class="text-[13px]">
-              <p class="font-bold text-VeryDarkGrayishBlue">
-                Michelle Appleton
-              </p>
-              <p class="text-GrayishBlue">28 Jun 2020</p>
+            <div class="flex gap-3">
+              <img src="images/icon-facebook.svg" alt="" />
+              <img src="images/icon-twitter.svg" alt="" />
+              <img src="images/icon-pinterest.svg" alt="" />
             </div>
           </div>
           <div>
             <button
               id="btn"
               type="button"
-              class="bg-VeryDarkGrayishBlue px-3 py-2 rounded-full"
+              class="bg-DesaturatedDarkBlue px-3 py-2 rounded-full"
             >
               <ion-icon
                 class="text-LightGrayishBlue"
@@ -34,7 +34,7 @@ btn.addEventListener("click", () => {
               ></ion-icon>
             </button>
           </div>
-          </div>
-        </div>`;
+        </div>
+      </div>`;
   footerContainer.style.display = "block";
 });
