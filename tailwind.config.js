7dff8d00faad0c9d35c2356ec086d222a1ff6cb8
lost50U1 <91626314+lost50U1@users.noreply.github.com>
2024-06-29 18:07:04 +0300

    all theme set

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     colors: {
-      "primary-red": "hsl(0, 78%, 62%)",
-      "primary-cyan": "hsl(180, 62%, 55%)",
-      "primary-orange": "hsl(34, 97%, 64%)",
-      "primary-blue": "hsl(212, 86%, 64%)",
-      "very-dark-blue": "hsl(234, 12%, 34%)",
-      "grayish-blue": "hsl(229, 6%, 66%)",
-      "very-light-gray": "hsl(0, 0%, 98%)",
+      VeryDarkGrayishBlue: "hsl(217, 19%, 35%)",
+      DesaturatedDarkBlue: "hsl(214, 17%, 51%)",
+      GrayishBlue: "hsl(212, 23%, 69%)",
+      LightGrayishBlue: "hsl(210, 46%, 95%)",
     },
     fontFamily: {
-      poppins: "'Poppins', sans-serif;",
+      poppins: "'Manrope', sans-serif;",
     },
     extend: {},
   },
