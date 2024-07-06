0e7e95686e2a665ab97628e4e648ca31a6e58f52
lost50U1 <91626314+lost50U1@users.noreply.github.com>
2024-07-06 21:57:25 +0300

    final-commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,22 @@
-const btn = document.getElementById("btn");
-const footerContainer = document.getElementById("footer-container");
+const btn = document.querySelector("#btn");
+const btn2 = document.querySelector("#btn2");
+const btn3 = document.querySelector("#btn3");
+const footerContainer = document.querySelector("#footer-container");
 const mobileContainer = document.getElementById("footer-container2");
+const tooltip = document.querySelector("#tooltip");
 
 btn.addEventListener("click", () => {
-  if()
-  footerContainer.classList.add("hidden");
-  mobileContainer.classList.remove("hidden");
-  /* create two variables with 1 with mobile layout 2 with desktop layout and make it check for sm: class with if else
-    method two create two footers with different id and create another footer variable
-    and improving it instead of using template literal make two footers one for mobile and one for
-    desktop on the html and make it none and block with js and by adding hidden class using tailwind*/
+  tooltip.classList.toggle("hidden");
+});
 
-  // Todo: new method
-  /*
-  add: add a class (if it’s not already there).
-remove: remove a class (if it is already there).
-toggle: remove the class if it’s there and add it if not.
-Favour style updates rather than modifying the DOM
-button.className = 'active' // className is 'active'
-button.classList.add('hidden') // className is 'active hidden'
-button.classList.add('hidden') // className is still 'active hidden'
-button.classList.toggle('active') // className is 'hidden'
-button.classList.toggle('alert') // className is 'hidden alert'
-button.classList.remove('hidden') // className is 'alert'
-and use arrow function
-    */
+btn2.addEventListener("click", () => {
+  mobileContainer.classList.toggle("hidden");
+  footerContainer.classList.toggle("hidden");
+  btn2.classList.toggle("hidden");
+});
+
+btn3.addEventListener("click", () => {
+  mobileContainer.classList.toggle("hidden");
+  footerContainer.classList.toggle("hidden");
+  btn2.classList.toggle("hidden");
 });
